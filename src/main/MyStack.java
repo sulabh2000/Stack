43024c51c0208b8e4ee7a752911c49edb2095855
sulabh2000 <bansalsulabh13@gmail.com>
2019-01-22 15:11:14 +0530

    some changes

--- src/main/MyStack.java
+++ src/main/MyStack.java
@@ -1,15 +1,57 @@
 package main;
 
+import java.util.Stack;
+
 public class MyStack
 {
     int size;
-    int top;
-    int stackarray[];
-}
+    int topofstack=-1;
+    int[] stackarray;
+    public MyStack(int size)
+    {
+        this.size=size;
+        stackarray=new int[size];
+    }
+    void push(int value)
+    {
+            // todo check if array is already full
+        if(isFull() == false)
+        {
 
-public MyStack()
-{
-    size=10;
-    top=-1;
-    stackarray=new int[size];
+        }
+    }
+    boolean isFull()
+    {
+        boolean response = false;
+        if(stackarray[stackarray.length - 1] != 0 && topofstack == stackarray.length - 1)
+        {
+            response = true;
+        }
+        return response;
+    }
+    int pop()
+    {
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        Stack stack1 = new Stack( size: 10);
+        for(int length =stack1.stackarray.length-1; length >= 0;length--)
+        {
+            System.out.println(stack1.stackarray[length]);
+        }
+        for(int i=0;i<10;i++)
+        {
+            stack1.push(value:40 + i);
+        }
+        System.out.println("--------");
+        for(int length =stack1.stackarray.length-1; length >= 0;length--)
+        {
+            System.out.println(stack1.stackarray[length]);
+        }
+    }
+
+
 }
